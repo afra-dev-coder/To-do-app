c82a4a64b8f965e0ec1e99774eb2868ca1e3a68d
afra-dev-coder <afraah7238@gmail.com>
2026-01-23 08:22:27 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function addTask(){
+    let newTask = document.createElement('li')
+    let taskList = document.getElementById('taskList')
+    taskList.appendChild(newTask)
+    newTask.textContent = document.getElementById('inputTask').value
+    document.getElementById('inputTask').value = ""
+    deleteTask(newTask)
+}
+
+function deleteTask(newTask){
+      let deleteBtn = document.createElement('button')
+      deleteBtn.textContent = "Delete"
+      newTask.appendChild(deleteBtn)
+      deleteBtn.onclick = function(){
+        newTask.remove()
+      }
+}
